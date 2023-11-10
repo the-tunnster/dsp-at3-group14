eaa8cd7572c8c9912048d68545184efc00d04a93
Able Varghese <vargheseablet@gmail.com>
2023-11-10 20:41:36 +1100

    First implementation of datetime tab

--- tab_date/logics.py
+++ tab_date/logics.py
@@ -1,34 +1,13 @@
+# tab_date/logics.py
+
 import pandas as pd
 import altair as alt
 
 class DateColumn:
-    """
-    --------------------
-    Description
-    --------------------
-    -> DateColumn (class): Class that manages a column from a dataframe of datetime data type
-
-    --------------------
-    Attributes
-    --------------------
-    -> file_path (str): Path to the uploaded CSV file (optional)
-    -> df (pd.Dataframe): Pandas dataframe (optional)
-    -> cols_list (list): List of columns names of dataset that are text type (default set to empty list)
-    -> serie (pd.Series): Pandas serie where the content of a column has been loaded (default set to None)
-    -> n_unique (int): Number of unique value of a serie (optional)
-    -> n_missing (int): Number of missing values of a serie (optional)
-    -> col_min (int): Minimum value of a serie (optional)
-    -> col_max (int): Maximum value of a serie (optional)
-    -> n_weekend (int): Number of times a serie has dates falling during weekend (optional)
-    -> n_weekday (int): Number of times a serie has dates not falling during weekend (optional)
-    -> n_future (int): Number of times a serie has dates falling in the future (optional)
-    -> n_empty_1900 (int): Number of times a serie has dates equal to '1900-01-01' (optional)
-    -> n_empty_1970 (int): Number of times a serie has dates equal to '1970-01-01' (optional)
-    -> barchart (int): Altair barchart displaying the count for each value of a serie (optional)
-    -> frequent (int): Dataframe containing the most frequest value of a serie (optional)
-
-    """
     def __init__(self, file_path=None, df=None):
+        """
+        Class constructor to initialize the DateColumn object.
+        """
         self.file_path = file_path
         self.df = df
         self.cols_list = []
@@ -47,324 +26,160 @@ class DateColumn:
     
     def find_date_cols(self):
         """
-        --------------------
-        Description
-        --------------------
-        -> find_date_cols (method): Class method that will load the uploaded CSV file as Pandas DataFrame and store it as attribute (self.df) if it hasn't been provided before.
-        Then it will find all columns of datetime data type. If it can't find any datetime then it will look for all columns of text time. Then it will store the results in the relevant attribute (self.cols_list).
-
-        --------------------
-        Parameters
-        --------------------
-        -> None
-
-        --------------------
-        Returns
-        --------------------
-        -> None
-
+        Method to find columns of datetime data type in the dataframe.
+        If no datetime columns are found, it looks for text columns.
         """
-        
+        if self.df is None:
+            if self.file_path is not None:
+                self.df = pd.read_csv(self.file_path)
+            else:
+                return
+
+        date_cols = self.df.select_dtypes(include=['datetime']).columns
+        if len(date_cols) == 0:
+            # If no datetime columns found, look for text columns
+            text_cols = self.df.select_dtypes(include=['object']).columns
+            self.cols_list = text_cols.tolist()
+        else:
+            self.cols_list = date_cols.tolist()
 
     def set_data(self, col_name):
         """
-        --------------------
-        Description
-        --------------------
-        --------------------
-        Description
-        --------------------
-        -> set_data (method): Class method that sets the self.serie attribute with the relevant column from the dataframe and then computes all requested information from self.serie to be displayed in the Date section of Streamlit app 
-
-        --------------------
-        Parameters
-        --------------------
-        -> col_name (str): Name of the text column to be analysed
-
-        --------------------
-        Returns
-        --------------------
-        -> None
+        Method to set data for analysis based on the selected column name.
         """
-        
+        if self.df is None:
+            return
+
+        if col_name in self.cols_list:
+            self.serie = self.df[col_name].dropna()  # Drop rows with null values
+            self.convert_serie_to_date()
+            self.set_unique()
+            self.set_missing()
+            self.set_min()
+            self.set_max()
+            self.set_weekend()
+            self.set_weekday()
+            self.set_future()
+            self.set_empty_1900()
+            self.set_empty_1970()
+            self.set_barchart()
+            self.set_frequent()
 
     def convert_serie_to_date(self):
         """
-        --------------------
-        Description
-        --------------------
-        -> convert_serie_to_date (method): Class method that convert a Pandas Series to datetime data type and store the results in the relevant attribute (self.serie).
-
-        --------------------
-        Parameters
-        --------------------
-        -> None
-
-        --------------------
-        Returns
-        --------------------
-        -> None
-
+        Method to convert a Pandas Series to datetime data type.
         """
-        
+        if self.serie is not None:
+            self.serie = pd.to_datetime(self.serie, errors='coerce')
 
     def is_serie_none(self):
         """
-        --------------------
-        Description
-        --------------------
-        -> is_serie_none (method): Class method that checks if self.serie is empty or none 
-
-        --------------------
-        Parameters
-        --------------------
-        -> None
-
-        --------------------
-        Returns
-        --------------------
-        -> (bool): Flag stating if the serie is empty or not
-
+        Method to check if self.serie is empty or None.
         """
-        
+        return self.serie is None
 
     def set_unique(self):
         """
-        --------------------
-        Description
-        --------------------
-        -> set_unique (method): Class method that computes the number of unique value of a serie and store the results in the relevant attribute(self.n_unique).
-
-        --------------------
-        Parameters
-        --------------------
-        -> None
-
-        --------------------
-        Returns
-        --------------------
-        -> None
-
+        Method to compute the number of unique values in a series.
         """
-        
+        if not self.is_serie_none():
+            self.n_unique = self.serie.nunique()
 
     def set_missing(self):
         """
-        --------------------
-        Description
-        --------------------
-        -> set_missing (method): Class method that computes the number of missing value of a serie and store the results in the relevant attribute(self.n_missing).
-
-        --------------------
-        Parameters
-        --------------------
-        -> None
-
-        --------------------
-        Returns
-        --------------------
-        -> None
-
+        Method to compute the number of missing values in a series.
         """
-        
+        if not self.is_serie_none():
+            self.n_missing = self.serie.isnull().sum()
 
     def set_min(self):
         """
-        --------------------
-        Description
-        --------------------
-        -> set_min (method): Class method that computes the minimum value of a serie and store the results in the relevant attribute(self.col_min).
-
-        --------------------
-        Parameters
-        --------------------
-        -> None
-
-        --------------------
-        Returns
-        --------------------
-        -> None
-
+        Method to compute the minimum value in a series.
         """
-        
+        if not self.is_serie_none():
+            self.col_min = self.serie.min()
 
     def set_max(self):
         """
-        --------------------
-        Description
-        --------------------
-        -> set_max (method): Class method that computes the minimum value of a serie and store the results in the relevant attribute(self.col_max).
-
-        --------------------
-        Parameters
-        --------------------
-        -> None
-
-        --------------------
-        Returns
-        --------------------
-        -> None
-
+        Method to compute the maximum value in a series.
         """
-        
+        if not self.is_serie_none():
+            self.col_max = self.serie.max()
 
     def set_weekend(self):
         """
-        --------------------
-        Description
-        --------------------
-        -> set_weekend (method): Class method that computes the number of times a serie has dates falling during weekend and store the results in the relevant attribute(self.n_weekend).
-
-        --------------------
-        Parameters
-        --------------------
-        -> None
-
-        --------------------
-        Returns
-        --------------------
-        -> None
-
+        Method to compute the number of times a series has dates falling during the weekend.
         """
-        
+        if not self.is_serie_none():
+            self.n_weekend = self.serie.dt.weekday.isin([5, 6]).sum()
 
     def set_weekday(self):
         """
-        --------------------
-        Description
-        --------------------
-        -> set_weekday (method): Class method that computes the number of times a serie has dates not falling during weekend and store the results in the relevant attribute(self.n_weekday).
-
-        --------------------
-        Parameters
-        --------------------
-        -> None
-
-        --------------------
-        Returns
-        --------------------
-        -> None
-
+        Method to compute the number of times a series has dates not falling during the weekend.
         """
-        
+        if not self.is_serie_none():
+            self.n_weekday = (~self.serie.dt.weekday.isin([5, 6])).sum()
 
     def set_future(self):
         """
-        --------------------
-        Description
-        --------------------
-        -> set_future (method): Class method that computes the number of times a serie has dates falling in the future and store the results in the relevant attribute(self.n_future).
-
-        --------------------
-        Parameters
-        --------------------
-        -> None
-
-        --------------------
-        Returns
-        --------------------
-        -> None
-
+        Method to compute the number of times a series has dates falling in the future.
         """
-        
-    
+        if not self.is_serie_none():
+            today = pd.to_datetime('today').normalize()
+            self.n_future = (self.serie > today).sum()
+
     def set_empty_1900(self):
         """
-        --------------------
-        Description
-        --------------------
-        -> set_empty_1900 (method): Class method that computes the number of times a serie has dates equal to '1900-01-01' and store the results in the relevant attribute(self.n_empty_1900).
-
-        --------------------
-        Parameters
-        --------------------
-        -> None
-
-        --------------------
-        Returns
-        --------------------
-        -> None
-
+        Method to compute the number of times a series has dates equal to '1900-01-01'.
         """
-        
+        if not self.is_serie_none():
+            self.n_empty_1900 = (self.serie == '1900-01-01').sum()
 
     def set_empty_1970(self):
         """
-        --------------------
-        Description
-        --------------------
-        -> set_empty_1970 (method): Class method that computes the number of times a serie has only digit characters and store the results in the relevant attribute(self.n_empty_1970).
-
-        --------------------
-        Parameters
-        --------------------
-        -> None
-
-        --------------------
-        Returns
-        --------------------
-        -> None
-
+        Method to compute the number of times a series has only numeric characters.
         """
-        
+        if not self.is_serie_none():
+            if self.serie.dtype.kind in 'iufc':  # Check if the dtype is numeric
+                self.n_empty_1970 = 0
+            else:
+                self.n_empty_1970 = self.serie.astype(str).str.isnumeric().sum()
 
-    def set_barchart(self):  
+
+    def set_barchart(self):
         """
-        --------------------
-        Description
-        --------------------
-        -> set_barchart (method): Class method that computes the Altair barchart displaying the count for each value of a serie and store the results in the relevant attribute(self.barchart).
-
-        --------------------
-        Parameters
-        --------------------
-        -> None
-
-        --------------------
-        Returns
-        --------------------
-        -> None
-
+        Method to compute the Altair barchart displaying the count for each value in a series.
         """
-        
-      
+        if not self.is_serie_none():
+            chart = alt.Chart(self.df).mark_bar().encode(
+                alt.X(f'{self.serie.name}:O', title='Date'),
+                alt.Y('count():Q', title='Count')  # Use Quantitative (numeric) encoding for Y-axis
+            )
+            self.barchart = chart
+
     def set_frequent(self, end=20):
         """
-        --------------------
-        Description
-        --------------------
-        -> set_frequent (method): Class method that computes the Dataframe containing the most frequest value of a serie and store the results in the relevant attribute(self.frequent).
-
-        --------------------
-        Parameters
-        --------------------
-        -> end (int):
-            Parameter indicating the maximum number of values to be displayed
-
-        --------------------
-        Returns
-        --------------------
-        -> None
-
+        Method to compute the dataframe containing the most frequent values in a series.
         """
-        
+        if not self.is_serie_none():
+            values_count = self.serie.value_counts().reset_index()
+            values_count.columns = ['value', 'occurrence']
+            values_count['percentage'] = values_count['occurrence'] / len(self.serie) * 100
+            self.frequent = values_count.head(end)
 
     def get_summary(self):
         """
-        --------------------
-        Description
-        --------------------
-        -> get_summary (method): Class method that formats all requested information from self.serie to be displayed in the Overall section of Streamlit app as a Pandas dataframe with 2 columns: Description and Value
-
-        --------------------
-        Parameters
-        --------------------
-        -> None
-
-        --------------------
-        Returns
-        --------------------
-        -> (pd.DataFrame): Formatted dataframe to be displayed on the Streamlit app
-
+        Method to format all requested information from self.serie for display.
+        Returns a Pandas dataframe with two columns: Description and Value.
         """
-        
+        data = {
+            'Description': ['Number of Unique Values', 'Number of Missing Values', 'Min Value', 'Max Value',
+                             'Number of Weekend Dates', 'Number of Weekday Dates', 'Number of Future Dates',
+                             "Number of '1900-01-01' Dates", 'Number of Numeric Dates'],
+            'Value': [self.n_unique, self.n_missing, self.col_min, self.col_max,
+                      self.n_weekend, self.n_weekday, self.n_future,
+                      self.n_empty_1900, self.n_empty_1970]
+        }
+
+        summary_df = pd.DataFrame(data)
+        return summary_df
