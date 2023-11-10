acbf3b2924c7a6c5a4b095d6a65a360d5c1bdcd7
vargheseablet <vargheseablet@gmail.com>
2023-11-10 12:49:40 +1100

    Add files via upload

--- tab_date/display.py
+++ tab_date/display.py
@@ -1,30 +1,36 @@
 import streamlit as st
-
 from tab_date.logics import DateColumn
 
 def display_tab_date_content(file_path=None, df=None):
-    """
-    --------------------
-    Description
-    --------------------
-    -> display_tab_date_content (function): Function that will instantiate tab_date.logics.DateColumn class, save it into Streamlit session state and call its tab_date.logics.DateColumn.find_date_cols() method in order to find all datetime columns.
-    Then it will display a Streamlit select box with the list of datetime columns found.
-    Once the user select a datetime column from the select box, it will call the tab_date.logics.DateColumn.set_data() method in order to compute all the information to be displayed.
-    Then it will display a Streamlit Expander container with the following contents:
-    - the results of tab_date.logics.DateColumn.get_summary() as a Streamlit Table
-    - the graph from tab_date.logics.DateColumn.histogram using Streamlit.altair_chart()
-    - the results of tab_date.logics.DateColumn.frequent using Streamlit.write
- 
-    --------------------
-    Parameters
-    --------------------
-    -> file_path (str): File path to uploaded CSV file (optional)
-    -> df (pd.DataFrame): Loaded dataframe (optional)
-
-    --------------------
-    Returns
-    --------------------
-    -> None
-
-    """
+   
+    # Instantiates the DateColumn object
+    if st.session_state.date_column is None:
+        st.session_state.date_column = DateColumn(file_path=file_path, df=df)
     
+    # Calls find_date_cols method to generate a list of datetime columns
+    st.session_state.date_column.find_date_cols()
+
+    # Dropdown list for datetime columns
+    st.session_state.selected_date_col = st.selectbox(
+        'Which datetime column do you want to explore',
+        st.session_state.date_column.cols_list
+    )
+
+    # Setting up the data for the selected column
+    st.session_state.date_column.set_data(col_name=st.session_state.selected_date_col)
+
+    # First Streamlit Expander container
+    with st.expander('Datetime Column', expanded=True):
+        # Display the summary as a Streamlit table
+        st.table(st.session_state.date_column.get_summary())
+
+        # Display the bar chart
+        st.write('**Bar Chart**')
+        st.altair_chart(st.session_state.date_column.barchart, use_container_width=True)
+
+        # Display the most frequent values dataframe
+        st.write('**Most Frequent Values**')
+        st.dataframe(st.session_state.date_column.frequent)
+
+if __name__ == '__main__':
+    display_tab_date_content()
